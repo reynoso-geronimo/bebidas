2074af52344516b815c146edb6b0abec8262d3a0
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-07-06 14:12:35 -0300

    Update RecetasContext.js
    Corregido consulta a la api y filtrado

--- src/context/RecetasContext.js
+++ src/context/RecetasContext.js
@@ -11,23 +11,44 @@ const RecetasProvider = (props) => {
   });
   const [consultar, guardarConsultar] = useState(false);
   //consultar api
-
+  const { nombre, categoria } = busqueda;
   useEffect(() => {
     if (consultar) {
-    const obtenerRecetas = async () => {
-      
-        const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${busqueda.nombre}&c=${busqueda.categoria}`;
+      const obtenerRecetas = async () => {
+        const urlC = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${categoria.replace(
+          " ",
+          "_"
+        )}`;
+        const urlI = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${nombre.replace(
+          " ",
+          "_"
+        )}`;
+        console.log(busqueda.nombre, busqueda.categoria);
 
-        const respuesta = await axios.get(url);
-        const resultado=respuesta.data.drinks;
-        guardarRecetas(resultado)
-       
-      }
+        const respuestaC = await axios.get(urlC);
+        const respuestaI = await axios.get(urlI);
+        const resultadoC = respuestaC.data.drinks;
+        const resultadoI = respuestaI.data.drinks;
+        let filtro = [];
+        resultadoI.forEach((resultado) => {
+          filtro.push(resultado.idDrink);
+        });
+
+        let coincidencias = [];
+
+        for (let l = 0; l < filtro.length; l++) {
+          for (let i = 0; i < resultadoC.length; i++) {
+            if (resultadoC[i].idDrink === filtro[l]) {
+              coincidencias.push(resultadoC[i]);
+            }
+          }
+        }
+
+        guardarRecetas(coincidencias);
+      };
       obtenerRecetas();
-      
-    };
-    
-  }, [busqueda,consultar]);
+    }
+  }, [busqueda, consultar]);
 
   return (
     <RecetasContext.Provider
